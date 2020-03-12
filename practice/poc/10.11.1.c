5f87fb7f2edda9ce12b36474a486542aba044822
imbriansun <fddflyme@gmail.com>
2020-03-12 16:23:23 +0800

    finish poc 10.11.1

---
+++ practice/poc/10.11.1.c
@@ -0,0 +1,15 @@
+struct info{
+	int date;
+	int time;
+	struct tel_num send;
+	struct tel_num recive;
+	struct tel_num pay;
+};
+
+struct tel_num{
+	int region_num;
+	int ex_num;
+	int station_num;
+};
+
+
